894d06a7eab78d613a41d87a04a3678d99468099
boriyanc <boriya.nc07@gmail.com>
2025-12-10 23:37:18 +0100

    its repeating. I HATE THIS. I AM CRASHING OUT. PLS. JUST WORK!!!

--- constants.ts
+++ constants.ts
@@ -5,22 +5,22 @@ export const PASSWORD_PROMPT = "my amazing boy's 19th:)";
 
 export const BACKGROUND_MUSIC: Song = {
   title: "Romantic Piano",
-  url: "/media/seasons.mp3" 
+  url: "./media/seasons.mp3" 
 };
 
-export const CAKE_IMAGE_URL = "/media/photo/cake.png"; 
-export const CANDLE_IMAGE_URL = "/media/photo/candle.png";
+export const CAKE_IMAGE_URL = "./media/photo/cake.png"; 
+export const CANDLE_IMAGE_URL = "./media/photo/candle.png";
 
 // polaroid photos
 export const MEMORIES_LIST: StickerData[] = [
-  { id: 1, x: 0, y: 0, imgUrl: "/media/photo/roblox.png", text: "me carrying you in roblox:)", delay: 0, duration: 15, rotation: -5 },
-  { id: 2, x: 0, y: 0, imgUrl: "/media/photo/cat.jpg", text: "confession: i only love cats because you love them. co-parent 3 cats with me?", delay: 2, duration: 18, rotation: 3 },
-  { id: 3, x: 0, y: 0, imgUrl: "/media/photo/photobooth 1.jpg", text: "the countless photobooths we've gone to. we needdd more photos!", delay: 4, duration: 20, rotation: -2 },
-  { id: 4, x: 0, y: 0, imgUrl: "/media/photo/oppa.jpg", text: "i love shopping with you. keep sending your fits, i love them all! please dont feel like you cant show me:(", delay: 1, duration: 16, rotation: 6 },
-  { id: 5, x: 0, y: 0, imgUrl: "/media/photo/this day.jpg", text: "what happened this day... do you remember or are you a fake :0", delay: 3, duration: 19, rotation: -4 },
-  { id: 6, x: 0, y: 0, imgUrl: "/media/photo/nose!.jpg", text: "the little night calls we have yapping away", delay: 5, duration: 17, rotation: 2 },
-  { id: 7, x: 0, y: 0, imgUrl: "/media/photo/sidequests.jpg", text: "our big back side quests/adventures!", delay: 2, duration: 22, rotation: -3 },
-  { id: 8, x: 0, y: 0, imgUrl: "/media/photo/water festival.jpg", text: "your first true taste of being khmer: water festival! to more wet adventures together!", delay: 6, duration: 21, rotation: 4 },
+  { id: 1, x: 0, y: 0, imgUrl: "./media/photo/roblox.png", text: "me carrying you in roblox:)", delay: 0, duration: 15, rotation: -5 },
+  { id: 2, x: 0, y: 0, imgUrl: "./media/photo/cat.jpg", text: "confession: i only love cats because you love them. co-parent 3 cats with me?", delay: 2, duration: 18, rotation: 3 },
+  { id: 3, x: 0, y: 0, imgUrl: "./media/photo/photobooth 1.jpg", text: "the countless photobooths we've gone to. we needdd more photos!", delay: 4, duration: 20, rotation: -2 },
+  { id: 4, x: 0, y: 0, imgUrl: "./media/photo/oppa.jpg", text: "i love shopping with you. keep sending your fits, i love them all! please dont feel like you cant show me:(", delay: 1, duration: 16, rotation: 6 },
+  { id: 5, x: 0, y: 0, imgUrl: "./media/photo/this day.jpg", text: "what happened this day... do you remember or are you a fake :0", delay: 3, duration: 19, rotation: -4 },
+  { id: 6, x: 0, y: 0, imgUrl: "./media/photo/nose!.jpg", text: "the little night calls we have yapping away", delay: 5, duration: 17, rotation: 2 },
+  { id: 7, x: 0, y: 0, imgUrl: "./media/photo/sidequests.jpg", text: "our big back side quests/adventures!", delay: 2, duration: 22, rotation: -3 },
+  { id: 8, x: 0, y: 0, imgUrl: "./media/photo/water festival.jpg", text: "your first true taste of being khmer: water festival! to more wet adventures together!", delay: 6, duration: 21, rotation: 4 },
 ];
 
 // These are the 19 reasons revealed by the candles
